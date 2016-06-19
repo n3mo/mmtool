57d07d352538b931f0166dd0d0cc3c63473cd940
aaronbev79 <aaron@aaron-Lenovo-B570.(none)>
2016-06-18 22:29:32 -0400

    cleaned up js

--- extras/htdocs/js/custom.js
+++ extras/htdocs/js/custom.js
@@ -16,20 +16,10 @@ $(document).ready(function(){
     }
  });
 });
-//
-// $(document).ready(function(){
-//   $('#task select').change(function() {
-//     var selectedText = $("#task select option:selected").text();
-//     if(selectedText.includes("google-country-trends" || "twitter-locations")){
-//         $("#options p").removeClass("no-input");
-//         $("#output, #count, #date, #duration, #location, #language, #query, #user").addClass("no-input");
-//       }
-//   });
-// });
 
-////////////////////////////////////////////////////
-// tasks: google-country-trends, twitter-locations
-////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+// tasks: tumblr-blog-info, wikipedia-page-links, wikipedia-search, wikipedia-text, web-text
+//////////////////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function(){
  $('#task select').change(function() {
     var optionText = jQuery.inArray( $("#task select option:selected").text(),
@@ -40,19 +30,3 @@ $(document).ready(function(){
     }
  });
 });
-
-
-// $(document).ready(function(){
-//   $('#task select').change(function() {
-//     var selectedText = $("#task select option:selected").text();
-//       if (selectedText = "tumblr-blog-info" || "wikipedia-page-links" || "wikipedia-search" || "wikipedia-text" || "web-text") {
-//         $("#options p, #output, #count, #date, #duration, #location, #language, #user").addClass("no-input");
-//         $("#query").removeClass("no-input");
-//         $("#options p, #output, #count, #date, #duration, #location, #language, #user").addClass("no-input");
-//       }
-//   });
-// });
-//
-//
-//
-//
